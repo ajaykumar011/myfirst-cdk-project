00ec7d4f1d93931989fc647ca429e182dfdf282d
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-12-29 19:18:28 +0530

    update

--- myfirst_cdk_project/myfirst_cdk_project_stack.py
+++ myfirst_cdk_project/myfirst_cdk_project_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core
+from aws_cdk import aws_s3 as _s3
 
 
 class MyfirstCdkProjectStack(core.Stack):
@@ -7,5 +8,36 @@ class MyfirstCdkProjectStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        mybucket = _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="ajkumar011cloud-9999",
+            versioned=True,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=core.RemovalPolicy.DESTROY
+            )
+        mybucket2 = _s3.Bucket(
+            self,
+            "myBucketId2",
+            bucket_name="ajkumar011cloud-10000",
+            versioned=True,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=core.RemovalPolicy.DESTROY
+            )
         
+        
+        output_1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_name, 
+            description=f"my first bucket", 
+            export_name="myBucketOutput1"
+            )
+        output_2 = core.CfnOutput(
+            self,
+            "myBucketOutput2",
+            value=mybucket2.bucket_name
+            )
 
